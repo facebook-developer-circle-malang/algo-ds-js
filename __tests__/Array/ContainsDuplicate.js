a16e3c78f23658506039ae64a4f5a5f4af4728e6
dwicao <lutfiandwica@gmail.com>
2017-04-13 09:31:24 +0700

    test duplicate array value from string

--- __tests__/Array/ContainsDuplicate.js
+++ __tests__/Array/ContainsDuplicate.js
@@ -5,11 +5,23 @@ import containsDuplicate from '../../src/Array/ContainsDuplicate';
 test('return true if an array contains duplicate values', () => {
   const arr = [22, 10, 5, 22, 90];
 
-  expect(containsDuplicate(arr)).toBe(true);
+  expect(containsDuplicate(arr)).toBeTruthy();
 });
 
 test('return false if an array NOT contains duplicate values', () => {
   const arr = [22, 10, 5, 90];
 
-  expect(containsDuplicate(arr)).toBe(false);
+  expect(containsDuplicate(arr)).toBeFalsy();
+});
+
+test('return true if an array contains duplicate values from string', () => {
+  const arr = ['vanilla', 'chocolate', 'blueberry', 'rasberry', 'chocolate'];
+
+  expect(containsDuplicate(arr)).toBeTruthy();
+});
+
+test('return false if an array NOT contains duplicate values from string', () => {
+  const arr = ['vanilla', 'chocolate', 'blueberry', 'rasberry'];
+
+  expect(containsDuplicate(arr)).toBeFalsy();
 });
