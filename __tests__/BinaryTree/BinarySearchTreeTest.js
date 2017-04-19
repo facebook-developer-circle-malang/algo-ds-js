611955cbcd6ee98d3102fb2f8b3fca18c3bd6d26
Reyhan Sofian <computecoholic@gmail.com>
2017-04-19 07:55:40 +0700

    fix eslint issue on test

--- __tests__/BinaryTree/BinarySearchTreeTest.js
+++ __tests__/BinaryTree/BinarySearchTreeTest.js
@@ -3,16 +3,16 @@
 import BinarySearchTree from '../../src/BinaryTree/BinarySearchTree';
 
 describe('BST Test', () => {
-    it('should return [30, 20, 45, 10, 60] after performing level order traversal', () => {
-        const bst = new BinarySearchTree();
-        bst.add(30, 30);
-        bst.add(20, 20);
-        bst.add(45, 45);
-        bst.add(10, 10);
-        bst.add(60, 60);
+  it('should return [30, 20, 45, 10, 60] after performing level order traversal', () => {
+    const bst = new BinarySearchTree();
+    bst.add(30, 30);
+    bst.add(20, 20);
+    bst.add(45, 45);
+    bst.add(10, 10);
+    bst.add(60, 60);
 
-        const levelOrderTraversal = bst.levelOrderTraversal();
+    const levelOrderTraversal = bst.levelOrderTraversal();
 
-        expect(levelOrderTraversal).toEqual([30, 20, 45, 10, 60]);
-    });
+    expect(levelOrderTraversal).toEqual([30, 20, 45, 10, 60]);
+  });
 });
