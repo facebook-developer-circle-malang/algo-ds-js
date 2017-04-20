80b407732123384b035177c3b65ca75a882257d8
Reyhan Sofian <computecoholic@gmail.com>
2017-04-20 10:34:01 +0700

    reverse sentence

--- __tests__/String/ReverseString.js
+++ __tests__/String/ReverseString.js
@@ -9,9 +9,23 @@ test('Word "Hola" should be reversed to "aloH"', () => {
   expect(reverseString).toEqual('aloH');
 });
 
+test('Sentence "Hola aloha" should be reversed to "ahola aloH"', () => {
+  const word = 'Hola aloha';
+  const reverseString = ReverseString(word);
+
+  expect(reverseString).toEqual('ahola aloH');
+});
+
 test('Word "Hola" should not be reversed to "aloh"', () => {
   const word = 'Hola';
   const reverseString = ReverseString(word);
 
   expect(reverseString).not.toEqual('aloh');
+});
+
+test('Sentence "Hola aloha" should not be reversed to "aloH ahola"', () => {
+  const word = 'Hola aloha';
+  const reverseString = ReverseString(word);
+
+  expect(reverseString).not.toEqual('aloH ahola');
 });
--- src/String/ReverseString.js
+++ src/String/ReverseString.js
@@ -1,12 +1,46 @@
 // @flow
 
 /**
- * Function that takes a string as input and returns the string reversed.
- * This function cannot reverse a string in a word.
+ * Reverse a word.
  *
  * @param  {string} string
  * @returns string
  */
-const ReverseString = (string: string): string => string.split('').reverse().join('');
+const reverseWord = (string: string): string => string.split('').reverse().join('');
+
+/**
+ * Check if the string is a sentence or not.
+ *
+ * @param  {string} string
+ * @returns boolean
+ */
+const isSentence = (string: string): boolean => /\s/g.test(string);
+
+/**
+ * Reverse the word in a sentence, also reverse the order of the words.
+ *
+ * @param  {string} sentence
+ */
+const reverseSentence = (sentence: string): string => {
+  const splitSentence = sentence.split(' ');
+
+  return splitSentence.map((word: string): string => reverseWord(word)).reverse().join(' ');
+};
+
+/**
+ * Function that takes a string as input and returns the string / sentence reversed.
+ * This function can also reverse a sentence.
+ *
+ * @param  {string} string
+ * @returns string
+ */
+const ReverseString = (string: string): string => {
+  if (isSentence(string)) {
+    return reverseSentence(string);
+  }
+
+  return reverseWord(string);
+};
+
 
 export default ReverseString;
